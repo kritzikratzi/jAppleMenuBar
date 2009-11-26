cb44eb611760945c6e78d43e50cd62665132bc67
hansi <super@superduper.org>
2009-11-26 18:32:05 +0100

    initial checkin -- read on:
    the code is in a terrible shape right now.
    however, it somehow compiles to something useable
    i will fix this ... one day, i guess...

---
+++ src/java/com/example/jAppleMenuBar.java
@@ -0,0 +1,30 @@
+//
+//  jAppleMenuBar.java
+//  jAppleMenuBar
+//
+//  Created by hansi on 25.11.09.
+//  Copyright (c) 2009 __MyCompanyName__. All rights reserved.
+//
+//	For information on setting Java configuration information, including 
+//	setting Java properties, refer to the documentation at
+//		http://developer.apple.com/techpubs/java/java.html
+//
+
+package com.example;
+
+import javax.swing.SwingUtilities;
+
+import com.example.app.ApplicationController;
+
+/**
+ * Starting point for the application. General initialization should be done inside
+ * the ApplicationController's init() method. If certain kinds of non-Swing initialization
+ * takes too long, it should happen in a new Thread and off the Swing event dispatch thread (EDT).
+ * 
+ * @author hansi
+ */
+public class jAppleMenuBar {
+	public static void main(final String[] args) {
+		System.out.println( "OKAY!" ); 
+	}
+}
